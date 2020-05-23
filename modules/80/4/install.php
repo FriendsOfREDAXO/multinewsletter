c91f8a61267b094338f0ed69bc01f1102df0f60b
Tobias Krais <tk@design-to-use.de>
2020-05-23 21:48:35 +0200

    use rex_version

--- modules/80/4/install.php
+++ modules/80/4/install.php
@@ -4,7 +4,7 @@ if(!rex_addon::get('yform')->isAvailable()) {
 	print rex_view::error(rex_i18n::msg('d2u_helper_modules_error_yform'));
 	$return = $return ? FALSE : $return;
 }
-else if (rex_string::versionCompare(rex_addon::get('yform')->getVersion(), '3.0', '<')) {
+else if (rex_version::compare(rex_addon::get('yform')->getVersion(), '3.0', '<')) {
 	print rex_view::error(rex_i18n::msg('multinewsletter_config_install_yform3_required'));
 	$return = $return ? FALSE : $return;
 }
