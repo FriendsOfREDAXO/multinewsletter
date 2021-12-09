45f83d015cf04e7978883dfd97672cae9ff06752
Tobias Krais <tk@design-to-use.de>
2021-12-09 12:18:12 +0100

    better style

--- modules/80/4/input.php
+++ modules/80/4/input.php
@@ -156,7 +156,7 @@
 </div>
 <div class="row">
 	<div class="col-xs-4">
-		<input type="checkbox" name="REX_INPUT_VALUE[2]" value="true" <?php echo "REX_VALUE[2]" == 'true' ? ' checked="checked"' : ''; ?> style="float: right;" />
+		<input type="checkbox" name="REX_INPUT_VALUE[2]" value="true" <?php echo "REX_VALUE[2]" == 'true' ? ' checked="checked"' : ''; ?> class="form-control d2u_helper_toggle" />
 	</div>
 	<div class="col-xs-8">
 		Im Formular auch nach dem Namen fragen?
