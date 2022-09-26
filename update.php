88f92d0ee0ae6fbbd4d376000ebdad0f3318e275
Tobias Krais <tk@design-to-use.de>
2022-09-26 14:47:53 +0200

    fix bug

--- update.php
+++ update.php
@@ -1,4 +1,5 @@
 <?php
+$sql = rex_sql::factory();
 // Datenbankengine auf Redaxo Standard umstellen
 $sql->setQuery('ALTER TABLE  ' . rex::getTablePrefix() . '375_archive ENGINE = INNODB;');
 $sql->setQuery('ALTER TABLE  ' . rex::getTablePrefix() . '375_group ENGINE = INNODB;');
