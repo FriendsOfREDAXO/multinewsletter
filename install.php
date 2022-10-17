0c6f0dbcda0a97a9aa7fc9116ac5138290c5b5dd
Tobias Krais <tk@design-to-use.de>
2022-10-17 18:08:18 +0200

    update module version

--- install.php
+++ install.php
@@ -93,7 +93,7 @@ if(class_exists('D2UModuleManager')) {
 		6);
 	$d2u_multinewsletter_modules[] = new D2UModule("80-4",
 		"MultiNewsletter YForm Anmeldung",
-		4);
+		5);
 	$d2u_multinewsletter_modules[] = new D2UModule("80-5",
 		"MultiNewsletter YForm Abmeldung",
 		2);
