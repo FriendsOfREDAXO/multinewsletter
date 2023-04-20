b6c06330dea818bbcbb8e00659fcb0a394f1bc7a
Tobias Krais <tk@design-to-use.de>
2023-04-20 12:19:07 +0200

    wrong type fixed

--- lib/group.php
+++ lib/group.php
@@ -47,7 +47,7 @@ class MultinewsletterGroup
             $this->default_sender_email = (string) $result->getValue('default_sender_email');
             $this->default_sender_name = (string) $result->getValue('default_sender_name');
             $this->reply_to_email = (string) $result->getValue('reply_to_email');
-            $this->default_article_id = (string) $result->getValue('default_article_id');
+            $this->default_article_id = (int) $result->getValue('default_article_id');
             $default_article = rex_article::get((int) $this->default_article_id);
             if ($default_article instanceof rex_article) {
                 $this->default_article_name = $default_article->getName();
