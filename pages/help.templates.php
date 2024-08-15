6bc99c3692428b337ff2d8338fc1e4c845e9362b
Tobias Krais <tk@design-to-use.de>
2024-08-15 16:05:36 +0200

    add echo

--- pages/help.templates.php
+++ pages/help.templates.php
@@ -6,7 +6,7 @@
 	<?php
 		$file_content = file_get_contents(rex_path::addon('multinewsletter') . 'templates/template_01.php');
 		if (false !== $file_content) {
-			rex_string::highlight($file_content);
+			echo rex_string::highlight($file_content);
 		}
 	?>
 </fieldset>
